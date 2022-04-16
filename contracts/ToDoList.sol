d7fe7b731c80149537118bf7d6e1638bc619d502
MarkMastro <mastropaolo.mark@gmail.com>
2022-04-16 15:52:13 -0400

    finished smart contract skeleton, made html page to display information

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.5.0;
-
+//use truffle migrate --reset to deploy new copy of smart contract if you already have it running and made some changes
 contract ToDoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Enter Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
---
+++ src/app.js
@@ -0,0 +1,5 @@
+App = {
+    load: async () => {
+        //load app
+    }
+}
